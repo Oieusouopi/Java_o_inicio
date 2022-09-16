a5a6f1b1e00879b1618eb965a081c0baf4d6d5f9
Oieusouopi <rafasc866@gmail.com>
2022-09-16 15:11:05 -0300

    add: ternario e comparacao

---
+++ Ternario/src/main/java/ternario/Ternario.java
@@ -0,0 +1,11 @@
+package ternario;
+
+public class Ternario {
+
+    public static void main(String[] args) {
+        int n1 = 2;
+        int n2 = 1;
+        int maior = n1 > n2 ? n1 : n2;
+        System.out.printf("O numero maior é " + maior);
+    }
+}
---
+++ comparacoString/src/main/java/comparacostring/ComparacoString.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package comparacostring;
+
+/**
+ *
+ * @author rafael
+ */
+public class ComparacoString {
+
+    public static void main(String[] args) {
+        String nome1 = "Gustavo";
+        String nome2 = "Gustavo";
+        String nome3 = new String("Gustavo");
+        String res;
+        res = nome3 == nome1 ? "igual": "diferente";
+        System.out.print(res);
+    }
+}
